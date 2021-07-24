c8085e34cf5aeb0c631e127cfdcc97b5567f7565
Magnus Nissel <magnus.nissel@gmail.com>
2021-07-24 22:37:40 +0200

    added functionality to delete files that are undersized on disk compared to the header Content-Length  (use with caution)

--- cli.py
+++ cli.py
@@ -4,7 +4,7 @@ import fire
 from typing import  Optional
 import hashlib
 
-from helpers import determine_data_dir, get_file_size_info_str
+from helpers import convert_size_to_str, determine_data_dir, get_file_size_info_str
 import downloading
 import extraction
 import processing
@@ -36,7 +36,8 @@ def _read_file_hash(fp:pathlib.Path):
 
 
 
-def list_files(downloaded:bool=True, extracted:bool=False, folder:Optional[str]=None, verify:bool=False, delete_mismatched:bool=False, delete_empty:bool=False) -> None:
+def list_files(downloaded:bool=True, extracted:bool=False, folder:Optional[str]=None, verify:bool=False, delete_mismatched:bool=False,
+               delete_empty:bool=False, delete_undersized:bool=False, size_ratio=0.8) -> None:
     if verify is True:
         logging.info("Downloading the most recent checksum file")
         check_fp = downloading.download_checksum_file("comments")
@@ -71,7 +72,33 @@ def list_files(downloaded:bool=True, extracted:bool=False, folder:Optional[str]=
                     if not fp.is_file():
                         logging.warning(f"Deleted file with invalid checksum: {fp}")
             if delete_empty is True and fp.stat().st_size == 0:
-                logging.warning(f"Deleted 0 byte file: {fp}")
+                try:
+                    fp.unlink()
+                except FileNotFoundError:
+                    pass
+                else:
+                    logging.warning(f"Deleted 0 byte file: {fp}")
+            elif delete_undersized is True:  # Check the url again for the approximate expected file size, if the file is significantly below it, then delete it
+                url = f"https://files.pushshift.io/reddit/comments/{fp.name}"
+                try:
+                    dl_file_size = downloading._check_url_content_length(url)
+                except TypeError:  # e.g. when url does not exist and function returns None
+                    pass
+                else:
+                    file_size = fp.stat().st_size
+                    if size_ratio > 0.99:
+                        size_ratio = 0.99
+                    elif size_ratio < 0.1:
+                        size_ratio = 0.1
+                    threshold = size_ratio * dl_file_size
+                    if file_size < threshold:
+                        logging.warning(f"Downloaded file is more than {(1-size_ratio)*100}% smaller than expected. (Expected {convert_size_to_str(dl_file_size)}, found {convert_size_to_str(file_size)}")
+                        try:
+                            fp.unlink()
+                        except FileNotFoundError:
+                            pass
+                        else:
+                            logging.warning(f"Deleted undersized file: {fp}")
 
 
     if extracted is True:
